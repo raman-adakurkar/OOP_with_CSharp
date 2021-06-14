2aaa779b8abbd5be9ca661270b0d59b964e8423d
ramanLadakurkar <ramanadakurkar@gmail.com>
2021-06-14 19:43:49 +0530

    Class and Object practiced

---
+++ ClassAndObjects/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    public class Ninja
+    {
+        // Initializing properties for class
+        public int height;
+        public int weight;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating object for Ninja class
+            Ninja raman = new Ninja();
+
+            // Accessing properties of object raman from Ninja Class
+            raman.height = 174;
+            raman.weight = 74;
+
+            // Displaying raman on console with properties
+            Console.WriteLine("Raman's Height = " + raman.height + "\nRaman's Weight = " + raman.weight);
+            Console.WriteLine();
+        }
+    }
+}
