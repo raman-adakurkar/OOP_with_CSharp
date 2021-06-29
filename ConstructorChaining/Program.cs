11d05b8d25dbc63e9b44426e479bfdc67d1c4dec
ramanLadakurkar <ramanadakurkar@gmail.com>
2021-06-29 12:00:41 +0530

    static and constructor chaining

---
+++ ConstructorChaining/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    public class Mobile
+    {
+        private int a;
+        private int b;
+
+        private string d;
+        private string e;
+        private string name;
+
+        public int A 
+        { 
+            get
+            {
+                return this.a;
+            }
+
+            set
+            {
+                this.a = value;
+            }
+        }
+        public int B
+        {
+            get
+            {
+                return this.b;
+            }
+
+            set
+            {
+                this.b = value;
+            }
+        }
+        public string D
+        {
+            get
+            {
+                return this.d;
+            }
+
+            set
+            {
+                this.d = value;
+            }
+        }
+        public string E
+        {
+            get
+            {
+                return this.e;
+            }
+
+            set
+            {
+                this.e = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public Mobile()
+            : this (10,20)
+        {
+
+        }
+
+        public Mobile(int a, int b)
+            : this(10, 20, "D", "E")
+        {
+
+        }
+
+        public Mobile(int a, int b, string d, string e)
+        {
+            this.A = a;
+            this.B = b;
+            this.D = "D changed";
+            this.E = e;
+            this.Name = "Raman";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Mobile();
+            Console.WriteLine(a.A);
+            Console.WriteLine(a.B);
+            Console.WriteLine(a.D);
+            Console.WriteLine(a.E);
+            Console.WriteLine(a.Name);
+        }
+    }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    public static class Practice
+    {
+        private static int number;
+        private static string name;
+
+        public static int Number
+        {
+            get
+            {
+                return number;
+            }
+
+            set
+            {
+                number = value;
+            }
+        }
+        public static string Name 
+        { 
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        static Practice()
+        {
+           Number = 10;
+           Name = "Raman";
+           Console.WriteLine(Number);
+           Console.WriteLine(Name);
+        }
+    }
+
+    public class Raman
+    {
+        private int a;
+
+        public int A 
+        {
+            get
+            {
+                return this.a;
+            }
+
+            private set
+            {
+                this.a = value;
+            }
+        }
+
+        public Raman(int a)
+        {
+            this.A = a;
+            Console.WriteLine(A);
+        }
+    }
+
+    enum E
+    {
+        one,
+        two
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice.Number = 100;
+            Practice.Name = "Raman Adakurkar";
+            Console.WriteLine();
+
+            Console.WriteLine(Practice.Name);
+            Console.WriteLine(Practice.Number);
+            Console.WriteLine();
+
+            var r = new Raman(10);
+            Console.WriteLine(r.A);
+
+            Console.WriteLine();
+            Console.WriteLine(E.two);
+
+
+
+        }
+    }
+}
